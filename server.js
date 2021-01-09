2005d08fdc37c309b37d625027fd9e2ef5436a71
Alize Hassan <alizehssn@gmail.com>
2021-01-09 12:41:39 -0600

    Server Bugs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const routes = require("./routes");
-const PORT = process.env.port || 3006;
+const PORT = process.env.PORT || 3006;
 const app = express();
 app.use(morgan("combined"));
 app.use(express.urlencoded({extended: true}));
