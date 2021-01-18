2e87a481fb28f5beaabbc1cdf726d1f09df510b1
Alize Hassan <alizehssn@gmail.com>
2021-01-18 10:15:15 -0600

    Fixed Date Format and revived app

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -2,35 +2,21 @@ import React from "react";
 
  function JobCard(props){
   console.log(props)
-
-  const date = new Date(props.job.jobDate).toLocaleString();
-
-  const moolah = (props.job.income)
-
-  const expenses =(props.job.expenses)
-
-  const tips = (props.job.tip)
-
-  const cashMoney = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(moolah)
-  
-  const Expense = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(expenses)
-
-  const $$$ = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(tips)
-
+  const dateObject = new Date(props.job.jobDate)
+  console.log(dateObject)
   return(
       <div className="card">
       <div className="card-header">
-          {props.job.jobTitle}
+           {props.job.jobTitle}
       </div>
 <ul className="list-group list-group-flush">
-  <li className="list-group-item">{date}</li>
-  <li className="list-group-item">Income: {cashMoney}</li>
-  <li className="list-group-item">Expenses: {Expense}</li>
-  <li className="list-group-item">Tips: {$$$}</li>
+  <li className="list-group-item">Date: {dateObject.toString().slice(0, 15)} </li>
+  <li className="list-group-item">Income: ${props.job.income}</li>
+  <li className="list-group-item">Expenses: ${props.job.expenses}</li>
+  <li className="list-group-item">Tips: ${props.job.tip}</li>
 </ul>
+
 </div>
   )
-
 }
-
 export default JobCard
